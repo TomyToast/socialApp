7318a0c1e552e69c3d1cb709668e02dcc713e190
Tomek Kulczyński <kulczynski.t@gmail.com>
2019-11-27 20:36:02 +0100

    organising

---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyACXMS7GwrLhC6UydbEzSDlIdBMYScg7NQ",
+    authDomain: "socialape-a74d3.firebaseapp.com",
+    databaseURL: "https://socialape-a74d3.firebaseio.com",
+    projectId: "socialape-a74d3",
+    storageBucket: "socialape-a74d3.appspot.com",
+    messagingSenderId: "896900813344",
+    appId: "1:896900813344:web:654f6883e8e6a513"
+};
